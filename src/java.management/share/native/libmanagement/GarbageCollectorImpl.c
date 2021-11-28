8a58034f354364b3a8107bfceae2bb7100f77fc2
tom.edge <tom.edge@autotrader.co.uk>
2021-11-28 21:25:59 +0000

    revert formatting change

--- src/java.management/share/native/libmanagement/GarbageCollectorImpl.c
+++ src/java.management/share/native/libmanagement/GarbageCollectorImpl.c
@@ -28,11 +28,11 @@
 #include "sun_management_GarbageCollectorImpl.h"
 
 JNIEXPORT jlong JNICALL Java_sun_management_GarbageCollectorImpl_getCollectionCount
-        (JNIEnv *env, jobject mgr) {
-  return jmm_interface->GetLongAttribute(env, mgr, JMM_GC_COUNT);
+  (JNIEnv *env, jobject mgr) {
+    return jmm_interface->GetLongAttribute(env, mgr, JMM_GC_COUNT);
 }
 
 JNIEXPORT jlong JNICALL Java_sun_management_GarbageCollectorImpl_getCollectionTime
-        (JNIEnv *env, jobject mgr) {
-  return jmm_interface->GetLongAttribute(env, mgr, JMM_GC_TIME_MS);
+  (JNIEnv *env, jobject mgr) {
+    return jmm_interface->GetLongAttribute(env, mgr, JMM_GC_TIME_MS);
 }
